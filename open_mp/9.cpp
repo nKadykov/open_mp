3f26fe2171bb7028fea724bf3cb72f735235ee52
nKadykov <nkadykov@gmail.com>
2024-05-21 14:39:29 +0300

    9

--- open_mp/9.cpp
+++ open_mp/9.cpp
@@ -1,22 +1,44 @@
 #include <omp.h>
 #include <iostream>
 #include <windows.h>
+#include <vector>
 #include <iomanip>
 
 int main() {
 	int k = 4;
-	int N;
-	std::cout << "Enter N: ";
-	std::cin >> N;
-	double pi = 0;
-#pragma omp parallel reduction(+:pi) num_threads(k)
-	{
-#pragma omp for schedule(guided, 2)
-		for (int i = 0; i < N; ++i) {
-			double x = (i + 0.5) / N;
-			pi += 4 / (1 + x * x) / N;
+	int n;
+	std::cout << "Enter n: ";
+	std::cin >> n;
+	std::cout << "Enter " << n * n * 2 << " numbers:";
+	std::vector<std::vector<double>> vector_a(n, std::vector<double>(n));
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			std::cin >> vector_a[i][j];
 		}
 	}
-	std::cout.precision(9);
-	std::cout << "Sum: " << pi;
+	std::vector<std::vector<double>> vector_b(n, std::vector<double>(n));
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			std::cin >> vector_b[i][j];
+		}
+	}
+	std::vector<std::vector<double>> vector_c(n, std::vector<double>(n));
+#pragma omp parallel num_threads(k)
+	{
+#pragma omp for schedule(guided, 2)
+		for (int i = 0; i < n; ++i) {
+			for (int m = 0; m < n; ++m) {
+				for (int j = 0; j < n; ++j) {
+					vector_c[i][m] += vector_a[i][j] * vector_b[j][m];
+				}
+			}
+		}
+	}
+	std::cout << "Result:\n";
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			std::cout << vector_c[i][j] << ' ';
+		}
+		std::cout << '\n';
+	}
 }
