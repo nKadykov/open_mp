def22f7745426a241bd453278bf14e204cf92b3d
nKadykov <nkadykov@gmail.com>
2024-05-21 15:02:41 +0300

    11

---
+++ open_mp/11.cpp
@@ -0,0 +1,22 @@
+#include <omp.h>
+#include <iostream>
+#include <windows.h>
+
+int main() {
+	int k = 4;
+	std::cout << "k: ";
+	std::cin >> k;
+	int N = 10;
+	std::cout << "N: ";
+	std::cin >> N;
+	int sum = 0;
+	#pragma omp parallel reduction(+:sum) num_threads(k)
+	{
+		#pragma omp for
+		for (int i = 1; i <= N; ++i) {
+			#pragma omp atomic
+			sum += i;
+		}
+	}
+	std::cout << sum;
+}
